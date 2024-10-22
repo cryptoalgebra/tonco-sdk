15d9cdf4427a09a02032b0c75ac7865640a376b5
damnnou <skdamn@outlook.com>
2024-10-22 12:16:26 +0300

    (1.0.8) ⌨️🥫🦡🍂 deploy, fix pTON wallet

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,4 +1,4 @@
 export const ROUTER = 'EQDk_t0nc0hpuEFn4Q83E0Gml30oMfjVKVIQf0bpHGo-6EDr';
 export const pTON_MINTER = 'EQBlde83gVhXQKGogwCxKbiyB37N5K0mX4YcC9XTR1iFX1iO';
 export const pTON_ROUTER_WALLET =
-  'EQB97RByzraH4zX-CAgvEFGbGXBKfr0iIl8r5c_-jkVECJV';
+  'EQB97RByzraH4zX-CAgvEFGbGXBKfr0iIl8r5c_-jkVECJVs';
