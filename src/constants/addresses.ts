8311ef39aeed6cbc00dfcc8f3bc85ba4dd0281b7
damnnou <skdamn@outlook.com>
2024-11-11 12:47:49 +0300

    (1.0.16) 🕹♌️🤱🌡  deploy, change pool factory

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,5 +1,5 @@
 export const ROUTER = 'EQDA-TONcOp2IIqfpkGnL2fInc-Cm-ZCS5uy_EF_B3etN1Uv';
-export const POOL_FACTORY = 'EQADPFq2PjhXb-Bl3riUDBONfxXrWccqX-1-HDXDovp6X4nT';
+export const POOL_FACTORY = 'EQDLTpnfAPo9n4TL0xXn1eyQ4OywdiSi5iXfhij7SsJbvj7J';
 export const pTON_MINTER = 'EQBlde83gVhXQKGogwCxKbiyB37N5K0mX4YcC9XTR1iFX1iO';
 export const pTON_ROUTER_WALLET =
   'EQAJpIhAKOlhedY781lKpxuKVAsmjxGsxWUMwz5BzzDlm17e';
