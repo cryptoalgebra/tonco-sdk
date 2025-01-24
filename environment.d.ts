a58904e6f1169ed39353f5f2f475e342a1cdf129
lilchizh <xlilchizhx@gmail.com>
2025-01-24 11:14:32 +0300

    (cicd) add chain deployments

---
+++ environment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        CHAIN: 'MAINNET' | 'TESTNET';
+        ROUTER: string;
+        POOL_FACTORY: string;
+        PTON_MINTER: string;
+        PTON_ROUTER: string;
+        ROUTER_CODE: string;
+      }
+    }
+  }
+
+export {}
--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,5 +1,4 @@
-export const ROUTER = 'EQC_-t0nCnOFMdp7E7qPxAOCbCWGFz-e3pwxb6tTvFmshjt5';
-export const POOL_FACTORY = 'EQDiNOe3qNffbCsEeX-6KYWT26TT1xL7D-dqx47-qqEkp4e9';
-export const pTON_MINTER = 'EQCUnExmdgwAKADi-j2KPKThyQqTc7U650cgM0g78UzZXn9J';
-export const pTON_ROUTER_WALLET =
-  'EQCHHakhWxSQIWbw6ioW21YnjVKBCDd_gVjF9Mz9_dIuFy23';
+export const ROUTER = process.env.ROUTER;
+export const POOL_FACTORY = process.env.POOL_FACTORY;
+export const pTON_MINTER = process.env.PTON_MINTER;
+export const pTON_ROUTER_WALLET = process.env.PTON_ROUTER;
