84a66ad87a54e5f1ff5fa7238841d3d4f73d8256
Kirill S <skdamn@outlook.com>
2025-05-29 11:07:54 +0300

    fix: add v1_5 pool code for testnet

--- environment.d.ts
+++ environment.d.ts
@@ -7,13 +7,16 @@ declare global {
       PTON_MINTER: string;
       PTON_ROUTER: string;
       ROUTER_CODE: string;
-      FARMING_DISTRIBUTOR: string;
-      TONCO_MIGRATE: string;
+      POOL_CODE: string;
       ROUTER_V1_5: string;
+      POOL_CODE_V1_5: string;
       POOL_FACTORY_V1_5: string;
       PTON_MINTER_V1_5: string;
       PTON_ROUTER_V1_5: string;
       ROUTER_CODE_V1_5: string;
+
+      FARMING_DISTRIBUTOR: string;
+      TONCO_MIGRATE: string;
     }
   }
 }
